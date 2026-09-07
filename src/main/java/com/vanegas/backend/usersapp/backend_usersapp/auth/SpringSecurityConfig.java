915e1335365dabf872208e56dba3145c0feb7cdb
izaivanegas <62458337+izaivanegas@users.noreply.github.com>
2026-09-07 09:21:42 -0600

    Agregue los metodos con sus respectivos permisos

--- src/main/java/com/vanegas/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
+++ src/main/java/com/vanegas/backend/usersapp/backend_usersapp/auth/SpringSecurityConfig.java
@@ -3,8 +3,11 @@ package com.vanegas.backend.usersapp.backend_usersapp.auth;
 
 import com.vanegas.backend.usersapp.backend_usersapp.auth.filters.JwtAuthenticationFilter;
 import com.vanegas.backend.usersapp.backend_usersapp.auth.filters.JwtValidationFilter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -13,6 +16,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -35,11 +45,16 @@ public class SpringSecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests(authz ->
                 authz.requestMatchers(HttpMethod.GET,"/users").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/{id}").hasAnyRole("ADMIN","USER","MANAGER")
+                        .requestMatchers(HttpMethod.POST,"/adduser").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/{id}").hasAnyRole("ADMIN","USER")
+                        .requestMatchers(HttpMethod.DELETE,"/{id}").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .addFilter(new JwtValidationFilter(authenticationManager()))
                 .csrf(csrf->csrf.disable())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors->cors.configurationSource(corsConfigurationSource()))
                 .build();
     }
 
@@ -49,6 +64,37 @@ public class SpringSecurityConfig {
     PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration =  new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        //ahora las rutas
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //para que se aplique a todas las rutas, se usa /**,
+        // si se quisiera aplicar a una ruta en especifico, se pondria /ruta/**
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean(){
+
+        FilterRegistrationBean<CorsFilter> bean =
+                new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        //bean.setFilter(new CorsFilter(corsConfigurationSource()));
+
+        return bean;
+    }
+
 
 
 }
